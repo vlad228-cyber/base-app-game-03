25d4532a2703ab77265d5c8cf349b1b95aef8106
vlad228-cyber <okxbybitbinance@gmail.com>
2026-02-05 02:12:32 +0300

    Use serverExternalPackages for externals

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  webpack: (config) => {
-    config.externals.push("pino-pretty", "lokijs", "encoding");
-    return config;
-  },
+  serverExternalPackages: ["pino-pretty", "lokijs", "encoding"],
 };
 
 export default nextConfig;
